0f52217b48ea8e6eb549111599dbbe5025732420
guptaRishi00 <rishigupta54066@gmail.com>
2025-08-14 01:08:21 +0530

    Initial commit

--- src/components/Preloader/index.js
+++ src/components/Preloader/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-import loaderImg from '../../assets/images/preload.png'
-
 const Preloader = () => {
   return (
     <div id="react__preloader">
 		<div id="react__circle_loader"></div>
 		<div className="react__loader_logo">
-			<img src={loaderImg} alt="Preload" />
+			<img src="/fav.png" alt="Loading" />
 		</div>
 	</div> 
   );
